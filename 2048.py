e75e2e25169ea54cff580e58fa1db817387593d1
Martin <martinjoseph690@gmail.com>
2022-02-06 17:58:09 +0530

    And initial random board state

--- 2048.py
+++ 2048.py
@@ -1,56 +1,71 @@
-size = 4
-
-# Game board
-mat = [[0, 0, 2, 4], [0, 0, 4, 8], [0, 2, 16, 32], [0, 2, 2, 16]]
-
-# Moves (left, right, up, down)
-d = ((0, -1), (0, 1), (-1, 0), (1, 0))
+import random
 
 # Check board bounds during iteration
-def inbounds(i, j):
+def inbounds(i, j, size):
     return all([i >= 0, i < size, j >= 0, j < size])
 
 
 # Print board
-def printMat(mat):
+def printMat(mat, size):
     for i in range(size):
         for j in range(size):
             print("\t", mat[i][j], end="")
         print()
 
 
-# Logic on move
-while True:
-    print("\n\n")
-    printMat(mat)
+def main():
+    size = 4
+    emptyCell = "."
 
-    # Get user input move
-    n = int(input("\n\nNext move: "))
-    if n not in (1, 2, 3, 4):
-        if n != 5:
-            print("\n\nInvalid input...")
-        print("\nExiting...")
-        break
+    # Game board
+    mat = [[emptyCell] * size for i in range(size)]
 
-    ip = d[n - 1]
+    # Moves (left, right, up, down)
+    d = ((0, -1), (0, 1), (-1, 0), (1, 0))
 
-    # Board state transition logic
-    for i in range(size):
-        for j in range(size):
-            m = i
-            n = j
-            num = mat[i][j]
-            while inbounds(m + ip[0], n + ip[1]) and mat[m + ip[0]][n + ip[1]] == 0:
-                m += ip[0]
-                n += ip[1]
+    x, y = random.randint(0, size - 1), random.randint(0, size - 1)
+    randNum = random.choice((2, 4))
+    mat[x][y] = randNum
 
-            if (
-                inbounds(m + ip[0], n + ip[1])
-                and mat[m + ip[0]][n + ip[1]] == mat[i][j]
-            ):
-                num = mat[m + ip[0]][n + ip[1]] * 2
-                m += ip[0]
-                n += ip[1]
+    # Logic on move
+    while True:
+        print("\n\n")
+        printMat(mat, size)
 
-            mat[i][j] = 0
-            mat[m][n] = num
+        # Get user input move
+        n = int(input("\n\nNext move: "))
+        if n not in (1, 2, 3, 4):
+            if n != 5:
+                print("\n\nInvalid input...")
+            print("\nExiting...")
+            break
+
+        ip = d[n - 1]
+
+        # Board state transition logic
+        for i in range(size):
+            for j in range(size):
+                m = i
+                n = j
+                num = mat[i][j]
+                while (
+                    inbounds(m + ip[0], n + ip[1], size)
+                    and mat[m + ip[0]][n + ip[1]] == emptyCell
+                ):
+                    m += ip[0]
+                    n += ip[1]
+
+                if (
+                    inbounds(m + ip[0], n + ip[1], size)
+                    and mat[m + ip[0]][n + ip[1]] == mat[i][j]
+                ):
+                    num = mat[m + ip[0]][n + ip[1]] * 2
+                    m += ip[0]
+                    n += ip[1]
+
+                mat[i][j] = emptyCell
+                mat[m][n] = num
+
+
+if __name__ == "__main__":
+    main()
